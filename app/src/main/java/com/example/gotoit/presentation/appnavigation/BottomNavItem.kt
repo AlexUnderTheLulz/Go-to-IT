87b7cef3a6a0a5d2bdf901a9ed9642c22e42280c
AlexUnderTheLulz <alexunderthelulz@outlook.com>
2025-03-09 00:18:03 +0300

    Calendar feature

--- app/src/main/java/com/example/gotoit/presentation/appnavigation/BottomNavItem.kt
+++ app/src/main/java/com/example/gotoit/presentation/appnavigation/BottomNavItem.kt
@@ -1,13 +1,12 @@
 package com.example.gotoit.presentation.appnavigation
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.DateRange
-import androidx.compose.material.icons.filled.Info
 import androidx.compose.ui.graphics.vector.ImageVector
 
+
 sealed class BottomNavItem (val route: String, val icon: ImageVector, val label: String) {
-    object Events: BottomNavItem("EventsScreen", Icons.Default.Info, "События")
+    object Events: BottomNavItem("EventsScreen", com.example.gotoit.presentation.theme.icons.vectorImages.Events, "События")
     object Calendar: BottomNavItem("calendarScreen", Icons.Default.DateRange, "Календарь")
-    object Home: BottomNavItem("homeScreen", Icons.Default.AccountCircle, "Новости")
+    object Home: BottomNavItem("homeScreen", com.example.gotoit.presentation.theme.icons.vectorImages.News, "Новости")
 }
